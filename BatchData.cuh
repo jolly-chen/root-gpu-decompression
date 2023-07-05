04075cf005e0e312cd25a593ac9d18e72764a296
Jolly Chen <jolly.chen@student.uva.nl>
2023-07-05 14:06:58 +0200

    Remove remnants of multi-file decompression

--- BatchData.cuh
+++ BatchData.cuh
@@ -14,46 +14,80 @@ inline static void __checkCudaErrors(cudaError_t error, std::string func, std::s
    }
 }
 
-static size_t compute_batch_size(const std::vector<std::vector<char>> &data, const size_t chunk_size)
+static size_t compute_batch_size(const std::vector<char> &data, const size_t chunk_size)
 {
-   size_t batch_size = 0;
-   for (size_t i = 0; i < data.size(); ++i) {
-      const size_t num_chunks = (data[i].size() + chunk_size - 1) / chunk_size;
-      batch_size += num_chunks;
-   }
-
-   return batch_size;
+   return (data.size() + chunk_size - 1) / chunk_size;
 }
 
-std::vector<size_t>
-compute_chunk_sizes(const std::vector<std::vector<char>> &data, const size_t batch_size, const size_t chunk_size)
+static std::vector<size_t>
+compute_chunk_sizes(const std::vector<char> &data, const size_t batch_size, const size_t chunk_size)
 {
    std::vector<size_t> sizes(batch_size, chunk_size);
 
-   size_t offset = 0;
-   for (size_t i = 0; i < data.size(); ++i) {
-      const size_t num_chunks = (data[i].size() + chunk_size - 1) / chunk_size;
-      offset += num_chunks;
-      if (data[i].size() % chunk_size != 0) {
-         sizes[offset - 1] = data[i].size() % chunk_size;
-      }
+   if (data.size() % chunk_size != 0) {
+      sizes[sizes.size() - 1] = data.size() % chunk_size;
    }
+
    return sizes;
+}
+
+static std::vector<void *>
+get_chunk_ptrs(char *data, const size_t *chunk_sizes, const size_t num_chunks)
+{
+   std::vector<void *> ptrs(num_chunks);
+   ptrs[0] = static_cast<void *>(data);
+   for (size_t i = 1; i < num_chunks; ++i) {
+      ptrs[i] = static_cast<void *>(data + chunk_sizes[i]);
+   }
+   return ptrs;
+}
+
+// For debugging
+__global__ void PrintBatch(void **chunk_pointers, size_t *chunk_sizes, char *data, size_t num_chunks)
+{
+   printf("------------------------- BATCH ----------------------------\n");
+   printf("num_chunks: %li\n", num_chunks);
+
+   printf("chunk_sizes:\n");
+   size_t total_size = 0;
+   for (int i = 0; i < num_chunks; i++) {
+      printf("%li ", chunk_sizes[i]);
+      total_size += chunk_sizes[i];
+   }
+   printf("\n");
+
+   printf("total_size: %li\n", total_size);
+
+   printf("data:\n");
+   for (int i = 0; i < total_size; i++) {
+      printf("%c ", data[i]);
+   }
+   printf("\n");
+
+   printf("chunk_pointers:\n");
+   for (int i = 0; i < num_chunks; i++) {
+      printf("\tchunk %d:\n\t\t", i);
+      for (int j = 0; j < chunk_sizes[i]; j++) {
+         printf("%c ", ((char *)chunk_pointers[i])[j]);
+      }
+      printf("\n");
+   }
+   printf("\n");
 }
 
 class BatchData {
 public:
    void **chunk_pointers;
    size_t *chunk_sizes;
-   uint8_t *data;
+   char *data;
    size_t num_chunks;
 
-   BatchData(const std::vector<std::vector<char>> &host_data, const size_t chunk_size, const size_t in_bytes)
+   BatchData(const std::vector<char> &host_data, const size_t chunk_size, const size_t in_bytes)
       : chunk_pointers(), chunk_sizes(), data(), num_chunks(0)
    {
       // Compute number of chunks
       num_chunks = compute_batch_size(host_data, chunk_size);
-      ERRCHECK(cudaMalloc(&data, chunk_size * num_chunks * sizeof(uint8_t)));
+      ERRCHECK(cudaMalloc(&data, chunk_size * num_chunks * sizeof(char)));
 
       // Compute size of each chunk
       std::vector<size_t> sizes = compute_chunk_sizes(host_data, num_chunks, chunk_size);
@@ -61,41 +95,23 @@ public:
       ERRCHECK(cudaMemcpy(chunk_sizes, sizes.data(), sizes.size() * sizeof(size_t), cudaMemcpyHostToDevice));
 
       // Copy data to GPU
-      ERRCHECK(cudaMalloc(&data, in_bytes * sizeof(uint8_t)));
-      size_t offset = 0;
-      for (size_t i = 0; i < host_data.size(); ++i) {
-         ERRCHECK(cudaMemcpy(&data[offset], host_data[i].data(), host_data[i].size(),
-                             cudaMemcpyHostToDevice));
-
-         const size_t num_chunks = (host_data[i].size() + chunk_size - 1) / chunk_size;
-         offset += num_chunks;
-      }
+      ERRCHECK(cudaMalloc(&data, in_bytes * sizeof(char)));
+      ERRCHECK(cudaMemcpy(data, host_data.data(), host_data.size(), cudaMemcpyHostToDevice));
 
       // Set up chunk pointers
-      std::vector<void *> ptrs(num_chunks);
-      for (size_t i = 0; i < num_chunks; ++i) {
-         ptrs[i] = static_cast<void *>(data + chunk_size * i);
-      }
+      auto ptrs = get_chunk_ptrs(data, chunk_sizes, num_chunks);
       ERRCHECK(cudaMalloc(&chunk_pointers, num_chunks * sizeof(void *)));
       ERRCHECK(cudaMemcpy(chunk_pointers, ptrs.data(), num_chunks * sizeof(void *), cudaMemcpyHostToDevice));
    }
 
-   BatchData(const size_t max_output_size, const size_t batch_size)
-      : chunk_pointers(), chunk_sizes(), data(), num_chunks(batch_size)
+   BatchData(size_t *h_chunk_sizes, size_t *d_chunk_sizes, const size_t num_chunks, const size_t total_size)
+      : chunk_pointers(), chunk_sizes(d_chunk_sizes), data(), num_chunks(num_chunks)
    {
       // Allocate space for data
-      ERRCHECK(cudaMalloc(&data, max_output_size * num_chunks * sizeof(uint8_t)));
-
-      // Compute size of each chunk
-      std::vector<size_t> sizes(num_chunks, max_output_size);
-      ERRCHECK(cudaMalloc(&chunk_sizes, sizes.size() * sizeof(size_t)));
-      ERRCHECK(cudaMemcpy(chunk_sizes, sizes.data(), sizes.size() * sizeof(size_t), cudaMemcpyHostToDevice));
+      ERRCHECK(cudaMalloc(&data, total_size * sizeof(char)));
 
       // Set up chunk pointers
-      std::vector<void *> ptrs(batch_size);
-      for (size_t i = 0; i < batch_size; ++i) {
-         ptrs[i] = data + max_output_size * i;
-      }
+      auto ptrs = get_chunk_ptrs(data, h_chunk_sizes, num_chunks);
       ERRCHECK(cudaMalloc(&chunk_pointers, ptrs.size() * sizeof(void *)));
       ERRCHECK(cudaMemcpy(chunk_pointers, ptrs.data(), ptrs.size() * sizeof(void *), cudaMemcpyHostToDevice));
    }
--- ll_decomp.cu
+++ ll_decomp.cu
@@ -7,46 +7,13 @@
 #include "nvcomp/zstd.h"
 #include "BatchData.cuh"
 
-// For debugging
-__global__ void PrintBatch(void **chunk_pointers, size_t *chunk_sizes, uint8_t *data, size_t num_chunks)
-{
-   printf("------------------------- BATCH ----------------------------\n");
-   printf("num_chunks: %li\n", num_chunks);
-
-   printf("chunk_sizes:\n");
-   size_t total_size = 0;
-   for (int i = 0; i < num_chunks; i++) {
-      printf("%li ", chunk_sizes[i]);
-      total_size += chunk_sizes[i];
-   }
-   printf("\n");
-
-   printf("total_size: %li\n", total_size);
-
-   printf("data:\n");
-   for (int i = 0; i < total_size; i++) {
-      printf("%c ", data[i]);
-   }
-   printf("\n");
-
-   printf("chunk_pointers:\n");
-   for (int i = 0; i < num_chunks; i++) {
-      printf("\tchunk %d:\n\t\t", i);
-      for (int j = 0; j < chunk_sizes[i]; j++) {
-         printf("%c ", ((char *)chunk_pointers[i])[j]);
-      }
-      printf("\n");
-   }
-   printf("\n");
-}
-
-void Decompress(const std::vector<std::vector<char>> &data, const size_t in_bytes)
+void Decompress(const std::vector<char> &data, const size_t in_bytes)
 {
    const size_t chunk_size = 1 << 16;
 
    // Copy compressed data to GPU
-   BatchData compress_data(data, chunk_size, in_bytes);
-   std::cout << "chunks: " << compress_data.num_chunks << std::endl;
+   BatchData comp_data(data, chunk_size, in_bytes);
+   std::cout << "chunks: " << comp_data.num_chunks << std::endl;
 
    // Create CUDA stream
    cudaStream_t stream;
@@ -59,62 +26,55 @@ void Decompress(const std::vector<std::vector<char>> &data, const size_t in_byte
 
    // Determine the size after decompression per chunk
    size_t *d_decomp_sizes = NULL;
-   ERRCHECK(cudaMalloc(&d_decomp_sizes, compress_data.num_chunks * sizeof(size_t)));
-   nvcompStatus_t status = nvcompBatchedZstdGetDecompressSizeAsync(
-      compress_data.chunk_pointers, compress_data.chunk_sizes, d_decomp_sizes, compress_data.num_chunks, stream);
+   ERRCHECK(cudaMallocAsync(&d_decomp_sizes, comp_data.num_chunks * sizeof(size_t), stream));
+   nvcompStatus_t status = nvcompBatchedZstdGetDecompressSizeAsync(comp_data.chunk_pointers, comp_data.chunk_sizes,
+                                                                   d_decomp_sizes, comp_data.num_chunks, stream);
    if (status != nvcompSuccess) {
       throw std::runtime_error("nvcompBatchedZstdDecompressGetTempSize() failed.");
    }
-   ERRCHECK(cudaStreamSynchronize(stream));
 
    // Allocate decompression buffer
-   size_t *decomp_sizes = (size_t *)malloc(compress_data.num_chunks * sizeof(size_t));
-   ERRCHECK(
-      cudaMemcpy(decomp_sizes, d_decomp_sizes, compress_data.num_chunks * sizeof(size_t), cudaMemcpyDeviceToHost));
+   size_t *h_decomp_sizes = (size_t *)malloc(comp_data.num_chunks * sizeof(size_t));
+   ERRCHECK(cudaMemcpyAsync(h_decomp_sizes, d_decomp_sizes, comp_data.num_chunks * sizeof(size_t),
+                            cudaMemcpyDeviceToHost, stream));
    size_t total_decomp_size = 0;
-   for (size_t i = 0; i < compress_data.num_chunks; ++i) {
-      total_decomp_size += decomp_sizes[i];
+   for (size_t i = 0; i < comp_data.num_chunks; ++i) {
+      total_decomp_size += h_decomp_sizes[i];
    }
-   BatchData decomp_data(total_decomp_size, compress_data.num_chunks);
+   BatchData decomp_data(h_decomp_sizes, d_decomp_sizes, comp_data.num_chunks, total_decomp_size);
 
    // Allocate temp space
-   size_t decomp_temp_bytes;
-   status = nvcompBatchedZstdDecompressGetTempSize(compress_data.num_chunks, chunk_size, &decomp_temp_bytes);
+   size_t d_decomp_temp_bytes;
+   status = nvcompBatchedZstdDecompressGetTempSize(comp_data.num_chunks, chunk_size, &d_decomp_temp_bytes);
    if (status != nvcompSuccess) {
       throw std::runtime_error("nvcompBatchedZstdDecompressGetTempSize() failed.");
    }
    void *d_decomp_temp = NULL;
-   ERRCHECK(cudaMalloc(&d_decomp_temp, decomp_temp_bytes));
+   ERRCHECK(cudaMallocAsync(&d_decomp_temp, d_decomp_temp_bytes, stream));
 
    // Status pointers
    nvcompStatus_t *d_status_ptrs = NULL;
-   ERRCHECK(cudaMalloc(&d_status_ptrs, decomp_data.num_chunks * sizeof(nvcompStatus_t)));
+   ERRCHECK(cudaMallocAsync(&d_status_ptrs, decomp_data.num_chunks * sizeof(nvcompStatus_t), stream));
 
    // Run decompression
-   status = nvcompBatchedZstdDecompressAsync(
-      compress_data.chunk_pointers, compress_data.chunk_sizes, decomp_data.chunk_sizes, d_decomp_sizes,
-      compress_data.num_chunks, d_decomp_temp, decomp_temp_bytes, decomp_data.chunk_pointers, d_status_ptrs, stream);
+   status = nvcompBatchedZstdDecompressAsync(comp_data.chunk_pointers, comp_data.chunk_sizes, decomp_data.chunk_sizes,
+                                             d_decomp_sizes, comp_data.num_chunks, d_decomp_temp, d_decomp_temp_bytes,
+                                             decomp_data.chunk_pointers, d_status_ptrs, stream);
    if (status != nvcompSuccess) {
       throw std::runtime_error("ERROR: nvcompBatchedGzipDecompressAsync() not successful");
    }
-   ERRCHECK(cudaStreamSynchronize(stream));
 
-   std::vector<std::vector<char>> uncompressed;
+   // Retrieve resuts
+   std::vector<char> uncompressed(total_decomp_size);
+   ERRCHECK(cudaMemcpyAsync(uncompressed.data(), decomp_data.data, total_decomp_size * sizeof(char),
+                            cudaMemcpyDeviceToHost, stream));
+   cudaStreamSynchronize(stream);
 
-   size_t offset = 0;
-   for (size_t i = 0; i < decomp_data.num_chunks; ++i) {
-      std::vector<char> result(decomp_sizes[i]);
-      ERRCHECK(cudaMemcpy(result.data(), &decomp_data.data[offset], decomp_sizes[i] * sizeof(char),
-                          cudaMemcpyDeviceToHost));
-      offset += decomp_sizes[i];
-      uncompressed.push_back(result);
+   std::cout << "---------------  RESULTS ------------------" << std::endl;
+   for (auto i = 0; i < uncompressed.size(); i++) {
+      fprintf(stdout, "%c", uncompressed[i]);
    }
-
-   for (auto file = 0; file < uncompressed.size(); file++) {
-      for (auto i = 0; i < uncompressed[file].size(); i++) {
-         fprintf(stdout, "%c", uncompressed[file][i]);
-      }
-   }
+   fprintf(stdout, "\n\n");
 
    cudaFree(d_decomp_temp);
 
@@ -144,43 +104,25 @@ std::vector<char> readFile(const std::string &filename)
    return host_data;
 }
 
-std::vector<std::vector<char>> multi_file(const std::vector<std::string> &filenames)
-{
-   std::vector<std::vector<char>> split_data;
-
-   for (auto const &filename : filenames) {
-      split_data.emplace_back(readFile(filename));
-   }
-
-   return split_data;
-}
-
 /**
  * Main
  */
 
 int main(int argc, char *argv[])
 {
-   std::vector<std::string> file_names(argc - 1);
+   std::string file_name;
 
    if (argc == 1) {
-      std::cerr << "Must specify at least one file." << std::endl;
+      std::cerr << "Must specify a file." << std::endl;
       return 1;
    }
 
-   // multi-file mode
-   for (int i = 1; i < argc; ++i) {
-      file_names[i - 1] = argv[i];
-   }
+   file_name = argv[1];
 
-   const std::vector<std::vector<char>> &data = multi_file(file_names);
+   const std::vector<char> &data = readFile(file_name);
    size_t input_buffer_len = data.size();
-   for (const std::vector<char> &part : data) {
-      input_buffer_len += part.size();
-   }
 
    std::cout << "----------" << std::endl;
-   std::cout << "files: " << data.size() << std::endl;
    std::cout << "compressed (B): " << input_buffer_len << std::endl;
 
    Decompress(data, input_buffer_len);
